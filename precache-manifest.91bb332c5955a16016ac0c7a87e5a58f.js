ef39df07312d25e6ccec620e8cf4d96034600ddb
cmac <chelsey.macneill@gmail.com>
2019-08-28 15:41:02 -0700

    Updates

---
+++ precache-manifest.91bb332c5955a16016ac0c7a87e5a58f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "97d143bd38c36b8e961e",
+    "url": "/clicky-game/static/js/main.e535ce22.chunk.js"
+  },
+  {
+    "revision": "759507250a9ccb04e254",
+    "url": "/clicky-game/static/js/2.e63a6107.chunk.js"
+  },
+  {
+    "revision": "97d143bd38c36b8e961e",
+    "url": "/clicky-game/static/css/main.456c36ae.chunk.css"
+  },
+  {
+    "revision": "9af7b8c4ca201962331f3c9661ced586",
+    "url": "/clicky-game/index.html"
+  }
+];
